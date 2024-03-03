e304ef5d0ea696478a105997354a689ad4e7465a
hana tm <117968852+Hana567tdss@users.noreply.github.com>
2024-03-03 10:50:11 -0800

    modify java code

--- frist.java
+++ frist.java
@@ -1,5 +1,5 @@
 public class main {
     public static main void(srting[] args){
-        system.out.println(" hello world")
+        system.out.println(" hello world ; this is something oohhooo")
     }
 }
